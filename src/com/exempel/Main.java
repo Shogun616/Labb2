efb2803f40313ced8769e9b9afddc17bf2a662d8
Philip Mattsson <maini.ac@hotmail.com>
2020-09-09 10:59:52 +0200

    Version 2.0

--- src/com/exempel/Main.java
+++ src/com/exempel/Main.java
@@ -4,77 +4,119 @@ import java.util.Scanner;
 
 public class Main {
 
+    static booksAdded[] books = new booksAdded[10];
     static boolean repeat = true;
-
     static Scanner scan = new Scanner(System.in);
-
     static String decision;
-
-  /*  static  void  options(){
+    static int counter = 0;
+    
+   public static  void  options(){
 
         boolean exit = true;
 
         while (exit) {
             decision = scan.nextLine();
 
-            if (decision.equalsIgnoreCase("nej")) {
+            if (decision.equalsIgnoreCase("no")) {
                 exit = false;
                 menu();
-            } else if (decision.equalsIgnoreCase("ja")) {
+            } else if (decision.equalsIgnoreCase("yes")) {
                 exit = false;
             }
         }
-    } */
-  /*  static void menu(){
-        System.out.println("\n======================================");
+    }
+
+    public static void menu() {
+
+        System.out.println("              BookStore              ");
+        System.out.println("======================================");
         System.out.println("              Main Menu             ");
         System.out.println("======================================");
-        System.out.println("1. Add");
-        System.out.println("2. Detail");
-        System.out.println("3. Update");
-        System.out.println("4. Delete");
+        System.out.println("1. Add Book");
+        System.out.println("2. Book Detail");
+        System.out.println("3. Update Book");
+        System.out.println("4. Delete Book");
         System.out.println("0. Exit");
 
         System.out.println("\nMake your choice");
         int choice = scan.nextInt();
         scan.nextLine();
 
-       switch (choice) {
+        switch (choice) {
 
             case 0:
-                System.out.println("Avsluta? (ja/nej)");
+                System.out.println("Exit? (yes/no)");
                 options();
+                System.out.println("This program has been terminated!");
                 repeat = false;
-                System.out.println("Avslutar spelet nu...");
                 break;
 
             case 1:
-
+                addBook();
                 break;
 
             case 2:
-
+                viewBook();
                 break;
 
             case 3:
-
+                updatebook();
                 break;
 
             case 4:
-
+                removeBook();
                 break;
 
-
             default:
-                System.out.println("Ogiltig val!");
+                System.out.println("Invalid options! Try again!");
 
-    } */
+        }
+    }
 
- /*   public static void main(String[] args) {
+   public static void main(String[] args) {
 
         while (repeat){
             menu();
         }
 
-    } */
+    }
+
+    public static void addBook(){
+
+        System.out.println("id: ");
+        int bookId = scan.nextInt();
+        scan.nextInt();
+
+        System.out.println("title: ");
+        String title = scan.nextLine();
+
+        System.out.println("Author: ");
+        String author = scan.nextLine();
+
+        System.out.println("Genre: ");
+        String genre = scan.nextLine();
+
+        System.out.println("Year: ");
+        String year = scan.nextLine();
+
+        System.out.println("Price: ");
+        double price = scan.nextInt();
+        scan.nextLine();
+
+        booksAdded b = new booksAdded(bookId, title, author, genre, year, price);
+        books[counter] =b;
+
+    }
+
+    public static void viewBook(){
+
+    }
+
+    public static void updatebook(){
+
+    }
+
+    public static void removeBook(){
+
+    }
 }
--- src/com/exempel/booksAdded.java
+++ src/com/exempel/booksAdded.java
@@ -2,16 +2,94 @@ package com.exempel;
 
 public class booksAdded {
 
+    protected int bookId;
     private String title;
     private String author;
     private String genre;
     private String year;
     private double price;
 
-    public booksAdded(){
+    private static String storeName = "Book ";
+
+    public booksAdded() {
     }
 
-    public booksAdded(String title, String author, String genre, String year, double price){
+    public booksAdded( int bookId, String title, String author, String genre, String year, double price){
 
+        if (price > 0){
+            this.price = price;
+        } else {
+            System.out.println("Price can not have a negative value");
+        }
+
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0){
+            this.price = price;
+        }else {
+            System.out.println("The price that you added have a negative value, please try again");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "booksAdded{" +
+                "bookId=" + bookId +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", year='" + year + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
